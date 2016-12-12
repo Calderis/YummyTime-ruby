41e412b57b640cd36028f399fa273422f6785f9c
Calderis <r.wetteren@gmail.com>
2016-12-12 11:07:42 +0000

    Food create > redirect all foods

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -37,7 +37,7 @@ class FoodsController < ApplicationController
 
     respond_to do |format|
       if @food.save
-        format.html { redirect_to @food, notice: 'Food was successfully created.' }
+        format.html { redirect_to foods_url, notice: 'Food was successfully created.' }
         format.json { render :show, status: :created, location: @food }
       else
         format.html { render :new }
