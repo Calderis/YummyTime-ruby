dabe7e1ca73b0c6600a3ec7eb27090e93b59096e
Calderis <r.wetteren@gmail.com>
2016-12-15 16:38:53 +0100

    fix routes famous recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -19,7 +19,7 @@ class RecipesController < ApplicationController
 
   # GET /cookbooks/famous.json
   def famous
-    hall_of_fames = Recipes.order(:count_time).first(20)
+    hall_of_fames = Recipe.order(:count_time).first(20)
     respond_to do |format|
       format.json { render :json => hall_of_fames, :methods => [:image] }
       format.html { redirect_to root_url }
